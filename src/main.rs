227a38895d53f66da11942749930048bae853646
Varunjay Varma <varunjay.varma@gmail.com>
2021-08-31 02:03:57 +0530

    Adding basic GET API using warp and tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+/*
+ * Currently just returning 200 OK with body "Hello name", on GET /hello/name
+ */
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+                .map(|name| format!("Hello, {}", name));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
